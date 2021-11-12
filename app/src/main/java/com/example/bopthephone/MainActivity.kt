c551438644be3c315c9dc20156f471df7d45601e
Rafael <rafael.schmer@studium.uni-hamburg.de>
2021-11-12 15:18:01 +0100

    gameclick added in main

--- app/src/main/java/com/example/bopthephone/MainActivity.kt
+++ app/src/main/java/com/example/bopthephone/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.bopthephone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun gameClick(view: View) {
+        val showGameView: Intent = Intent(this, GameActivity::class.java)
+        startActivity(showGameView)
     }
 }
